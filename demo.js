45ddce49b9514f97b90b181a4c6c507eac9cfd44
Jacob Groundwater <jacob@newrelic.com>
2014-04-27 13:21:06 -0700

    snapshot

--- demo.js
+++ demo.js
@@ -1,16 +1,34 @@
+var jtrace = require('./index.js')
+
+var mod1 = jtrace.get('mod1')
+var mod2 = jtrace.get('mod2')
+
 var i = 0;
 
-setInterval(function(){
-  console.dir  ('mod1', "dir message");
-  console.log  ('mod1', "log message");
-  console.info ('mod1', "info message");
-  console.warn ('mod1', "warn message");
-  console.error('mod1', "error message");
-  setTimeout(function () {
-    a(i++)
-  }, 100)
-}, 1000)
-
-function a(x) {
-  console.log('mod2', "I'm from another module" + x);
+function noise() {
+  return require('crypto').randomBytes(50).toString('hex');
 }
+
+setInterval(function(){
+  mod1.dir(noise());
+}, 10)
+
+setInterval(function(){
+  mod1.log(noise());
+}, 40)
+
+setInterval(function(){
+  mod1.info(noise());
+}, 100)
+
+setInterval(function(){
+  mod1.warn(noise());
+}, 200)
+
+setInterval(function(){
+  mod1.error(noise().substr(0,50) + 'ERROR' + noise().substr(0,45));
+}, 500)
+
+setInterval(function () {
+  mod2.error(noise());
+}, 5)
--- index.js
+++ index.js
@@ -7,22 +7,74 @@ var util  = require('util');
 
 function JTracer() {
   this.tracer = null;
+  this.parent = null;
+  this.module = null;
 }
 
-JTracer.prototype.emit = function (level, vector, items) {
-  if (this.tracer) {
-    this.tracer.trace(level, vector, items);
+JTracer.prototype.emit = function (level, vector, values) {
+
+  // always use root emitter
+  if (this.parent) {
+    this.parent.emit(level, this.module, values);
   }
+
+  else
+
+  if (this.tracer) {
+    var args = [];
+    for (var i in values) args.push(values[i])
+    this.tracer.trace(level, this.module + '/' + vector, args);
+  }
+};
+
+JTracer.prototype.log = function (a1, a2) {
+  this.emit(0, this.module, arguments);
+};
+
+JTracer.prototype.dir = function (a1, a2) {
+  this.emit(-10, this.module, arguments);
+};
+
+JTracer.prototype.info = function (a1, a2) {
+  this.emit(10, this.module, arguments);
+};
+
+JTracer.prototype.warn = function (a1, a2) {
+  this.emit(20, this.module, arguments);
+};
+
+JTracer.prototype.error = function (a1, a2) {
+  this.emit(30, this.module, arguments);
+};
+
+JTracer.prototype.get = function (mod) {
+  var jt = new JTracer()
+
+  jt.parent = this;
+  jt.module = mod;
+
+  return jt;
 };
 
 JTracer.prototype.start = function (path) {
   var self = this;
   http.createServer(function (req, res) {
+    var names = {};
+
+    names[-10] = '[DEBUG]';
+    names[0]   = '[  LOG]';
+    names[10]  = '[ INFO]';
+    names[20]  = '[ WARN]';
+    names[30]  = '[ERROR]';
 
     var sbox = {
       log: function(){
         var x = util.format.apply(null, arguments);
         res.write(x + '\n');
+      },
+      name: function (val) {
+        if (names[val]) return names[val]
+        else return ''
       }
     };
 
@@ -42,3 +94,4 @@ JTracer.prototype.start = function (path) {
 };
 
 module.exports = new JTracer();
+module.exports.module = '';
